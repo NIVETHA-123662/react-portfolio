7276bc2c8804722691e45ecfdf4a0c1ffbbf16e9
NIVETHA-123662 <nivethadhanalakshmi23@gmail.com>
2025-09-06 00:04:46 +0530

    modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route, Link } from "react-router-dom"
-import Home from "./pages/home"
+import Home from "./pages/Home"
 import About from "./pages/about"
 import Contact from "./pages/Contact"
 
